8b2b46935d0227beec338bf949ec2af15f26bab8
qiutong <tong.qiu@intel.com>
2024-09-24 08:20:55 +0000

    add memory debug function on win in main

--- melo.cpp
+++ melo.cpp
@@ -12,7 +12,19 @@
 #include <string>
 #include <cassert>
 #include <sstream>
+#include <format>
 #include <chrono>
+
+//#define DEBUG_MEMORY
+
+#if defined(_WIN32) && defined(DEBUG_MEMORY)
+#define PSAPI_VERSION 1 // PrintMemoryInfo 
+#include <windows.h>
+#include <psapi.h>
+#pragma comment(lib,"psapi.lib") //PrintMemoryInfod
+#include "processthreadsapi.h"
+#endif
+
 #include "src/openvoice2_processor.h"
 
 typedef std::chrono::high_resolution_clock Time;
@@ -20,6 +32,22 @@ typedef std::chrono::milliseconds ms;
 inline double get_duration_ms_till_now(Time::time_point& startTime) {
     return std::chrono::duration_cast<ms>(Time::now() - startTime).count();
 };
+#if defined(_WIN32) && defined(DEBUG_MEMORY)
+// To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
+// and compile with -DPSAPI_VERSION=1
+static void DebugMemoryInfo(const char* header)
+{
+    PROCESS_MEMORY_COUNTERS_EX2 pmc;
+    if (GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc)))
+    {
+        //The following printout corresponds to the value of Resource Memory, respectively
+        printf("%s:\tCommit \t\t\t=  0x%08X- %u (KB)\n", header, pmc.PrivateUsage, pmc.PrivateUsage / 1024);
+        printf("%s:\tWorkingSetSize\t\t\t=  0x%08X- %u (KB)\n", header, pmc.WorkingSetSize, pmc.WorkingSetSize / 1024);
+        printf("%s:\tPrivateWorkingSetSize\t\t\t=  0x%08X- %u (KB)\n", header, pmc.PrivateWorkingSetSize, pmc.PrivateWorkingSetSize / 1024);
+    }
+
+}
+#endif
 int main()
 {
     // set ONEDNN_CACHE_CAPACITY 
@@ -30,17 +58,16 @@ int main()
         int y;
         std::stringstream ss(onednn_kernel_capacity);
         ss >> y;
-        //std::cout << y << std::endl;
         assert((y==0) && "[ERROR] Set ONEDNN_PRIMITIVE_CACHE_CAPACITY fails!");
     }
     melo::MeloTTSProcessor* tts_processor =  new melo::MeloTTSProcessor();
 
     //fp16 model
-    std::string zh_tts_path = "thirdParty/tts_ov/tts_zn_mix_en.xml";
+    //std::string zh_tts_path = "thirdParty/tts_ov/tts_zn_mix_en.xml";
     //std::string zh_bert_path = "thirdParty/tts_ov/bert.xml";
 
     //int8 model
-    //std::string zh_tts_path = "thirdParty/tts_ov/tts_int8.xml";
+    std::string zh_tts_path = "thirdParty/tts_ov/tts_int8.xml";
     std::string zh_bert_path = "thirdParty/tts_ov/bert_int8.xml";
 
     // init tokenizer
@@ -48,20 +75,34 @@ int main()
     auto startTime = Time::now();
     tts_processor->LoadTTSModel(zh_tts_path, zh_bert_path, vocab_bert_path);
     auto execTime = get_duration_ms_till_now(startTime);
-    
-    std::cout << "Model Load Time:" << execTime << " milliseconds\n";
 
+    std::cout << "Model Load Time:" << execTime << " milliseconds\n";
+#if defined(_WIN32) && defined(DEBUG_MEMORY)
+    DebugMemoryInfo("Memory after model loading");
+#endif 
     std::vector<float> addit_param = { 0.2, 0.6, 1.0, 0.80 };
 
     std::string convert_text = "编译器compiler会尽可能从函数实参function arguments推导缺失的模板实参template arguments";
     std::vector<float> wav_data;
-    startTime = Time::now();
-    tts_processor->Process(convert_text, 0, addit_param, wav_data);
-    std::cout << "OV infer Time:" << execTime << " milliseconds\n";
+#if defined(_WIN32) && defined(DEBUG_MEMORY)
+    for (int i = 0; i < 50; ++i) {
+#endif 
+        startTime = Time::now();
+        tts_processor->Process(convert_text, 0, addit_param, wav_data);
+        execTime = get_duration_ms_till_now(startTime);
+        std::cout << "OV infer Time:" << execTime << " milliseconds\n";
+#if defined(_WIN32) && defined(DEBUG_MEMORY)
+        DebugMemoryInfo(std::format("memory after infer {} round", i).c_str());
+    }
+#endif 
+
 
     //16000 origin
     tts_processor->WriteWave("melo_tts_CPU.wav", 44100, wav_data.data(), wav_data.size());
     std::cout << "finish to generate wav" << std::endl;
+#if defined(_WIN32) && defined(DEBUG_MEMORY)
+    DebugMemoryInfo("memory afer save wav");
+#endif 
     //return 0;
 #ifdef CRT_
     _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
