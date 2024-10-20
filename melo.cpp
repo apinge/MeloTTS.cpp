c96a0f9665b95c8db1d6bda4afc837c4ab5e6216
qiutong <tong.qiu@intel.com>
2024-10-20 01:15:15 +0000

    change input args to use commandline args instead of hardcoded file paths in code

--- melo.cpp
+++ melo.cpp
@@ -1,11 +1,4 @@
 ﻿
-#ifdef CRT_
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-#endif
-
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -13,9 +6,6 @@
 #include <filesystem>
 
 #ifdef _WIN32
-//#include <codecvt>
-//#include <fcntl.h>
-//#include <io.h>
 #include <windows.h>
 #endif
 
@@ -32,6 +22,7 @@
 #include "utils.h"
 #include "tts.h"
 #include "chinese_mix.h"
+#include "parse_args.h"
 
 #if defined(_WIN32) && defined(DEBUG_MEMORY)
 // To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
@@ -48,66 +39,31 @@ static void DebugMemoryInfo(const char* header)
     }
 }
 #endif
-int main()
+int main(int argc, char** argv)
 {
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
 #endif
 
     ConfigureOneDNNCache();
+    Args args = parse_args(argc, argv);
 
-    //fp32 model
-    //std::filesystem::path zh_tts_path = "ov_models/tts_ZH_fp32.xml";
-    //std::filesystem::path zh_bert_path = "ov_models/bert_ZH_fp32.xml";
-    //fp16 model
-    /*std::filesystem::path zh_tts_path = "ov_models/tts_zn_mix_en.xml";
-    std::filesystem::path zh_bert_path = "ov_models/bert_zn_mix_en.xml";*/
-
-    //int8 model
-    std::filesystem::path zh_tts_path = "ov_models/tts_zn_mix_en_int8.xml"; //fp16 to int8
-    //std::filesystem::path zh_tts_path = "ov_models/tts_ZH_int8.xml"; //fp32 to int8
-    std::filesystem::path zh_bert_path = "ov_models/bert_zn_mix_en_int8.xml";
-    //std::filesystem::path zh_bert_path = "ov_models/bert_int8_ZH.xml";// model in python repo
-
-    // init tokenizer
-    std::filesystem::path vocab_bert_path = "ov_models/vocab_bert.txt";
-
-    // punctuation dict
-    std::filesystem::path punc_dict_path = "ov_models/punc.dic";
-
-    // dict folder for cppjieba
-   std::filesystem::path cppjieba_dict = "thirdParty/cppjieba/dict";
-
-   // cppinyin
-   std::filesystem::path cppinyin_resource = "thirdParty/cppinyin/resources/cpp_pinyin.raw";
-
-   //dict
-   std::filesystem::path cmudict_path = "ov_models/cmudict_cache.txt";
-
-   //pinyin_to_symbol_map
-   std::filesystem::path pinyin_to_symbol_map_path = "ov_models/opencpop-strict.txt";
-
-    // input files
-   std::filesystem::path input_path = "inputs.txt";
-    //outputpath
-    std::filesystem::path output_path = "audio.wav";
+    std::filesystem::path input_path = args.input_file;
+    std::filesystem::path output_path = args.output_file;
 
     //Init lanugage module
-    melo::chinese_mix::cmudict = std::make_shared<melo::CMUDict>(cmudict_path.string());
-    melo::chinese_mix::jieba = std::make_shared<cppjieba::Jieba>(cppjieba_dict);
-    melo::chinese_mix::pinyin_to_symbol_map = melo::chinese_mix::readPinyinFile(pinyin_to_symbol_map_path);
-    melo::chinese_mix::pinyin = std::make_shared<cppinyin::PinyinEncoder>(cppinyin_resource);
+    melo::chinese_mix::cmudict = std::make_shared<melo::CMUDict>(args.cmudict_path.string());
+    melo::chinese_mix::jieba = std::make_shared<cppjieba::Jieba>(args.cppjieba_dict);
+    melo::chinese_mix::pinyin_to_symbol_map = melo::chinese_mix::readPinyinFile(args.pinyin_to_symbol_map_path);
+    melo::chinese_mix::pinyin = std::make_shared<cppinyin::PinyinEncoder>(args.cppinyin_resource);
     std::cout <<"Init language Module\n";
 
     // Init core
     std::unique_ptr<ov::Core> core_ptr = std::make_unique<ov::Core>();
     auto startTime = Time::now();
-    melo::TTS model(core_ptr, zh_tts_path,"CPU",zh_bert_path,"CPU",vocab_bert_path, punc_dict_path, "ZH", false);
+    melo::TTS model(core_ptr, args.zh_tts_path,"CPU",args.zh_bert_path,"CPU",args.vocab_bert_path, args.punc_dict_path, "ZH", false);
     auto initTime = get_duration_ms_till_now(startTime);
     std::cout << "model init time is" << initTime <<" ms" << std::endl;
-#if defined(_WIN32) && defined(DEBUG_MEMORY)
-    DebugMemoryInfo("Memory after model loading");
-#endif 
 
     std::vector<std::string> texts = read_file_lines(input_path);
     for(int i = 0;i<1;++i){
@@ -116,25 +72,5 @@ int main()
         auto inferTime = get_duration_ms_till_now(startTime);
         std::cout << "model infer time:" << inferTime << " ms"<< std::endl;
     }
-#if defined(_WIN32) && defined(DEBUG_MEMORY)
-   for (int i = 0; i < 50; ++i) {
-#endif 
 
-#if defined(_WIN32) && defined(DEBUG_MEMORY)
-        DebugMemoryInfo(std::format("memory after infer {} round", i).c_str());
-    }
-#endif 
-
-
-
-    //tts_processor->WriteWave("melo_tts_CPU.wav", 44100, wav_data.data(), wav_data.size());
-    //std::cout << "finish to generate wav" << std::endl;
-#if defined(_WIN32) && defined(DEBUG_MEMORY)
-    DebugMemoryInfo("memory afer save wav");
-#endif 
-    //return 0;
-#ifdef CRT_
-    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
-    _CrtDumpMemoryLeaks();
-#endif
 }
---
+++ src/parse_args.h
@@ -0,0 +1,169 @@
+#pragma once
+#ifndef PARSE_ARGS_H
+#define PARSE_ARGS_H
+#include <filesystem>
+#include <iostream>
+#ifdef _WIN32
+#include <codecvt>
+#include <fcntl.h>
+#include <io.h>
+#include <windows.h>
+#endif
+struct Args
+{
+    std::filesystem::path model_dir = "ov_models";
+    std::string tts_device = "CPU";
+    std::string bert_device = "CPU";
+    std::string input_file = "inputs.txt";
+    std::string output_file = "audio.wav";
+    bool quantize = false;
+    bool disable_bert = false;
+    std::string language = "ZH";
+
+    void generate_init_file_paths();
+
+ 
+    std::filesystem::path zh_tts_path; //tts_model
+    std::filesystem::path zh_bert_path; //bert_model
+    std::filesystem::path vocab_bert_path;// init tokenizer
+    std::filesystem::path punc_dict_path;// // punctuation dict
+    std::filesystem::path cppjieba_dict;// dict folder for cppjieba
+    std::filesystem::path cppinyin_resource; // cppinyin
+    std::filesystem::path cmudict_path; // Carnegie Mellon University Pronouncing Dictionary, used for english pronunciation 
+    std::filesystem::path pinyin_to_symbol_map_path;//pinyin_to_symbol_map
+};
+
+inline void usage(const std::string& prog)
+{
+    std::cout << "Usage: " << prog << " [options]\n"
+        << "\n"
+        << "options:\n"
+        << "  --model_dir             the model folder\n"
+        << "  --tts_device            OpenVINO Device for tts model (default: CPU)\n"
+        << "  --bert_device           OpenVINO Device for bert model (default: CPU)\n"
+        << "  --input_file            input text file\n"
+        << "  --output_file           output audio file\n"
+        << "  --quantize              whether to use int8 model or not  (default: false, use fp16 model by default)\n"
+        << "  --disable_bert          whether to disable bert model infer (default: false)\n"
+        << "  --language              specify the language of TTS (default: ZH)\n";
+}
+
+static bool to_bool(const std::string& s) {
+    bool res;
+    std::istringstream(s) >> std::boolalpha >> res;
+    return res;
+}
+
+inline Args parse_args(const std::vector<std::string>& argv)
+{
+    Args args;
+
+    for (size_t i = 1; i < argv.size(); i++)
+    {
+        const std::string& arg = argv[i];
+
+        if (arg == "-h" || arg == "--help")
+        {
+            usage(argv[0]);
+            exit(EXIT_SUCCESS);
+        }
+        else if (arg == "--model_dir")
+        {
+            args.model_dir = argv[++i];
+        }
+        else if (arg == "--tts_device")
+        {
+            args.tts_device = argv[++i];
+        }
+        else if (arg == "--bert_device")
+        {
+            args.bert_device = argv[++i];
+        }
+        else if (arg == "--input_file")
+        {
+            args.input_file = argv[++i];
+        }
+        else if (arg == "--output_file")
+        {
+            args.output_file = argv[++i];
+        }
+        else if (arg == "--disable_bert")
+        {
+            args.disable_bert = to_bool(argv[++i]);
+        }
+        else if (arg == "--quantize")
+        {
+            args.quantize = to_bool(argv[++i]);
+        }
+        else if (arg == "--language")
+        {
+            args.language = argv[++i];
+        }
+        else
+        {
+            usage(argv[0]);
+            throw std::runtime_error("Unknown argument: " + arg);
+        }
+    }
+    args.generate_init_file_paths();
+    return args;
+}
+
+inline Args parse_args(int argc, char** argv)
+{
+    std::vector<std::string> argv_vec;
+    argv_vec.reserve(argc);
+
+#ifdef _WIN32
+    LPWSTR* wargs = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    for (int i = 0; i < argc; i++)
+    {
+        argv_vec.emplace_back(converter.to_bytes(wargs[i]));
+    }
+
+    LocalFree(wargs);
+#else
+    for (int i = 0; i < argc; i++)
+    {
+        argv_vec.emplace_back(argv[i]);
+    }
+#endif
+
+    return parse_args(argv_vec);
+}
+
+inline void Args::generate_init_file_paths() {
+    if (quantize) {
+        zh_tts_path = model_dir / "tts_zn_mix_en_int8.xml";
+        zh_bert_path = model_dir / "bert_zn_mix_en_int8.xml";
+    }
+    else {
+        //fp16 model
+        zh_tts_path = model_dir / "tts_zn_mix_en.xml";
+        zh_bert_path = model_dir / "bert_zn_mix_en.xml";
+    }
+
+    // init tokenizer
+    vocab_bert_path = model_dir / "vocab_bert.txt";
+
+    // punctuation dict
+    punc_dict_path = model_dir / "punc.dic";
+
+    //dict
+    cmudict_path = model_dir / "cmudict_cache.txt";
+
+    //pinyin_to_symbol_map
+    pinyin_to_symbol_map_path = model_dir / "opencpop-strict.txt";
+
+    // These two folders should ideally belong to the thirdParty directory.
+    // However, for convenience, they are placed under model_dir here.
+    // dict folder for cppjieba
+    cppjieba_dict = model_dir / "cppjieba/dict";
+    // cppinyin
+    cppinyin_resource = model_dir / "cppinyin/cpp_pinyin.raw";
+}
+
+
+#endif 
